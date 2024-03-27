eefed3f8982e898bbf0d6f5bfd8d8dadf65155bc
TaimaHamadneh <taimanizar45@gmail.com>
2024-03-28 01:34:54 +0200

    Book Store -Author and Book- CURD operation

---
+++ Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Bookstore.Models.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookstore
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddScoped<IBookstoreRepository<Author>, AuthorDbRepository>();
+            services.AddScoped<IBookstoreRepository<Book>, BookDbRepository>();
+
+            services.AddDbContext<BookstoreDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("SqlCon"));
+            });
+        }
+
+
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseRouting(); // Enable endpoint routing
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Book}/{action=Index}/{id?}");
+            });
+
+            /*
+            app.UseMvc(route => {
+                route.MapRoute("default", "{controller=Book}/{action=Index}/{id?}");
+            });*/
+        }
+    }
+}
